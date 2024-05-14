6e0bef50ed7acc2c09e17419e305acc3c8110d9f
seth <seth.russell@cuanschutz.edu>
2024-05-14 08:42:35 -0600

    Adding files from server

---
+++ nightly_rsync_backup.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo '-------------------------------------'
+echo `date`
+gitlab-backup create
+rsync -vrltDR --delete --delete-excluded --exclude-from=/root/rsync_exclusion_list.txt /home /etc /root /var/opt/gitlab/backups /media/data_mount/backup/servername_backup
+find /home -printf "%m:%u:%g:%p\n" > /root/`date +\%Y\%m\%d\%H\%M\%S`-home-perms-cron.log
+find /var/opt/gitlab/backups -printf "%m:%u:%g:%p\n" > /root/`date +\%Y\%m\%d\%H\%M\%S`-gitlab-perms-cron.log
+mv /root/*cron.log /media/data_mount/backup/servername_backup/logs/
+echo `date`
+echo '-------------------------------------'
